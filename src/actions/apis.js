db508d258cb63f9abb2529dad2305b97a80bc977
hanibhat <hanighulam@gmail.com>
2020-04-19 21:47:07 +0300

    conforming with api changes

--- src/actions/apis.js
+++ src/actions/apis.js
@@ -1,1 +1,2 @@
 export const REPOS_API = 'http://localhost:5000/api/repos';
+export const BOOKMARK_API = 'http://localhost:5000/api/bookmarks';
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import Axios from "axios"
 import { SEARCH_REPOSITORIES, FETCH_BOOKMARKS, BOOKMARK_REPO, REMOVE_BOOKMARK } from './types'
-import { REPOS_API } from './apis'
+import { REPOS_API, BOOKMARK_API } from './apis'
 
 /**
  * @description calls the search API and stores the returned array in repositories reducer
@@ -20,7 +20,7 @@ export const searchRepositories = query => dispatch => {
  * @description calls the bookmarks API and stores the returned array in bookmarks reducer
  */
 export const fetchBookmarks = () => dispatch => {
-  Axios.get(`${REPOS_API}/bookmarks`)
+  Axios.get(`${BOOKMARK_API}`)
     .then(res => dispatch({
       type: FETCH_BOOKMARKS,
       payload: res.data
@@ -32,7 +32,7 @@ export const fetchBookmarks = () => dispatch => {
  * @param {string} id The ID of the repository which will be added to the bookmarks array
  */
 export const bookmarkRepository = id => dispatch => {
-  Axios.post(`${REPOS_API}/bookmark/${id}`)
+  Axios.post(`${BOOKMARK_API}/${id}`)
     .then(res => {
       return res.data;
     }).then(repo => dispatch({
@@ -46,7 +46,7 @@ export const bookmarkRepository = id => dispatch => {
  * @param {string} id The ID of the repository to remove from bookmarks array
  */
 export const removeBookmark = id => dispatch => {
-  Axios.delete(`${REPOS_API}/bookmarks/${id}`)
+  Axios.delete(`${BOOKMARK_API}/${id}`)
     .then(res => {
       return res.data;
     }).then(repos => dispatch({
