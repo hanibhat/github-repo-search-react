4d9a06f40f7b6f02ca8743691271d7257ced9d38
hanibhat <hanighulam@gmail.com>
2020-08-09 15:08:53 +0200

    switched to async/await instead of then

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,51 +6,61 @@ import { REPOS_API, BOOKMARK_API } from './apis'
  * @description calls the search API and stores the returned array in repositories reducer
  * @param {string} query
  */
-export const searchRepositories = query => dispatch => {
-  Axios.get(`${REPOS_API}/search/?q=${query}`)
-    .then(res => {
-      return res.data;
-    }).then(repos => dispatch({
+export const searchRepositories = query => async dispatch => {
+  try {
+    const res = await Axios.get(`${REPOS_API}/search/?q=${query}`);
+    dispatch({
       type: SEARCH_REPOSITORIES,
-      payload: repos
-    })).catch(err => alert(err))
+      payload: res.data
+    });
+  } catch (err) {
+    alert(err)
+  }
 }
 
 /**
  * @description calls the bookmarks API and stores the returned array in bookmarks reducer
  */
-export const fetchBookmarks = () => dispatch => {
-  Axios.get(`${BOOKMARK_API}`)
-    .then(res => dispatch({
+export const fetchBookmarks = () => async dispatch => {
+  try {
+    const res = await Axios.get(BOOKMARK_API);
+    dispatch({
       type: FETCH_BOOKMARKS,
       payload: res.data
-    })).catch(err => alert(err))
+    });
+  } catch (err) {
+    alert(err)
+  }
 }
 
 /**
  * @description calls the bookmarks API, POSTS the repository as a new bookmark and stores the returned array in bookmarks reducer
  * @param {string} id The ID of the repository which will be added to the bookmarks array
  */
-export const bookmarkRepository = id => dispatch => {
-  Axios.post(`${BOOKMARK_API}/${id}`)
-    .then(res => {
-      return res.data;
-    }).then(repo => dispatch({
+export const bookmarkRepository = id => async dispatch => {
+  try {
+    const res = await Axios.post(`${BOOKMARK_API}/${id}`);
+    dispatch({
       type: BOOKMARK_REPO,
-      payload: repo
-    })).catch(err => alert(err))
+      payload: res.data
+    });
+  } catch (err) {
+    alert(err)
+  }
 }
 
 /**
  * @description calls the bookmarks API, removes the repository from bookmarks array and stores the returned array in bookmarks reducer
  * @param {string} id The ID of the repository to remove from bookmarks array
  */
-export const removeBookmark = id => dispatch => {
-  Axios.delete(`${BOOKMARK_API}/${id}`)
-    .then(res => {
-      return res.data;
-    }).then(repos => dispatch({
+export const removeBookmark = id => async dispatch => {
+  try {
+    const res = await Axios.delete(`${BOOKMARK_API}/${id}`);
+    dispatch({
       type: REMOVE_BOOKMARK,
-      payload: repos
-    })).catch(err => alert(err))
+      payload: res.data
+    });
+  } catch (err) {
+    alert(err)
+  }
 }
